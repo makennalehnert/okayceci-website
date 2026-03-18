ddee9f1f0f447f1822593ee0aa60f43a632682ec
Makenna <makennaiklehnert@gmail.com>
2026-03-18 18:59:29 -0500

    fixing art redirect in nav bar

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -8,7 +8,7 @@ export default function Art() {
   ]
 
   return (
-    <section id="merch" className="min-h-screen bg-pink-300 text-white p-10 scroll-mt-10 flex flex-col items-center">
+    <section id="art" className="min-h-screen bg-pink-300 text-white p-10 scroll-mt-10 flex flex-col items-center">
       <h2 className="text-4xl font-bitcount font-bold mb-10">Art</h2>
       <div className="flex mt-20">
        <LoadingDots/>
